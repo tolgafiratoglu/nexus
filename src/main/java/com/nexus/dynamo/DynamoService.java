cbbaef022592cf902cf00bcb6fe24b785281d195
tolgafiratoglu <tolgafiratoglu@gmail.com>
2023-07-06 16:23:01 +0300

    List dynamo tables in Dynamo Service

--- src/main/java/com/nexus/dynamo/DynamoService.java
+++ src/main/java/com/nexus/dynamo/DynamoService.java
@@ -9,9 +9,13 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.AmazonSNSClientBuilder;
-import com.amazonaws.services.sns.model.CreateTopicRequest;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 import com.amazonaws.services.sns.model.Topic;
 
 import java.io.InputStream;
@@ -19,41 +23,47 @@ import java.io.InputStream;
 @Service
 public class DynamoService {
     
-    AmazonSNS snsClient;
+    AmazonDynamoDB dynamoDbClient;
+    DynamoDB dynamoDB;
 
     DynamoService() {
         AWSCredentials credentials = new ProfileCredentialsProvider().getCredentials();
 
-        snsClient = AmazonSNSClientBuilder
+        dynamoDbClient = AmazonDynamoDBClientBuilder
             .standard()
             .withCredentials(new AWSStaticCredentialsProvider(credentials))
             .withRegion(Regions.US_EAST_1)
             .build();
     }
 
-    protected List<DynamoDTO> convertIntoDTOList(List<Bucket> buckets) {
-        List<DynamoDTO> bucketDTOList = new ArrayList<DynamoDTO>();
+    protected List<DynamoDTO> convertIntoDTOList(List<String> tableNames) {
+        List<DynamoDTO> dynamoDTOList = new ArrayList<DynamoDTO>();
 
-        for(Bucket bucket : buckets) {
-            DynamoDTO bucketDTO = new DynamoDTO();
-            bucketDTO.setName(bucket.getName());
-            bucketDTO.setDate(bucket.getCreationDate());
-            bucketDTOList.add(bucketDTO);
+        for(String tableName : tableNames) {
+            DynamoDTO dynamoDTO = new DynamoDTO();
+            dynamoDTO.setName(tableName);
+            dynamoDTOList.add(dynamoDTO);
         }
 
-        return bucketDTOList;
+        return dynamoDTOList;
     }
 
     protected List<DynamoDTO> getList() {
-        List<Bucket> buckets = s3Client.listBuckets();
-        return convertIntoDTOList(buckets);
+        ListTablesResult tables = dynamoDbClient.listTables();
+        List<String> tableNames = tables.getTableNames();
+        return convertIntoDTOList(tableNames);
     }
 
-    protected Topic create(String topicName) {
-        CreateTopicRequest request = CreateTopicRequest.builder()
-                .name(topicName)
-                .build();
+    protected Table create(String tableName) {
+        CreateTableRequest createRequest = new CreateTableRequest()
+                .withTableName(tableName);
+        
+        CreateTableResult createResult = dynamoDbClient.createTable(createRequest);
 
-        return snsClient.createTopic(CreateTopicRequest request);
+        // Wait for the table to be created
+        dynamoDbClient.waiters().tableExists().run(() -> new DescribeTableRequest(tableName));
+
+        // Get the newly created table
+        return dynamoDB.getTable(tableName);
     }
 }
